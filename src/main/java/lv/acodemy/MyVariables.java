0fd7392e6c9fad4b017c0f00f09b576f5b465169
Aleksejs Matrjonins <venijskitalec63@gmail.com>
2024-11-14 16:20:27 +0200

    (feature/twp-1337); learn Variables

---
+++ src/main/java/lv/acodemy/MyVariables.java
@@ -0,0 +1,90 @@
+package lv.acodemy;
+
+public class MyVariables {
+    public static void main(String[] args) {
+
+            // This is my comment
+
+            /*
+            This is first line
+            This is second line
+            This is third line
+             */
+
+            // int (integer) - whole number / nan
+            int age = 31;
+            int currentYear = 2024;
+            int inventoryItemCount = 100;
+
+            System.out.println(age);
+            System.out.println(currentYear);
+            System.out.println(inventoryItemCount);;
+
+            // double (fractional numbers)
+            double temperature = 36.6;
+            double price = 19.99;
+            double distance = 17500.95;
+
+            System.out.println(temperature);
+            System.out.println(price);;
+
+            // char (symbols)
+            char grade = 'A';
+            char myInitial = 'M';
+            char currencySymbol = '$';
+
+            System.out.println(grade);
+            System.out.println(myInitial);
+            System.out.println(currencySymbol);
+
+            // boolean (logical type, can keep only two values: true or false)
+            // How to name? has or is
+            // Example: isSummer / if(isSummer) {}; isLoggedIn
+            // Example: hasAccess / if(hasAccess) {};
+
+            boolean hasAccess = true;
+            System.out.println(hasAccess);
+
+            boolean isSummer = false;
+            System.out.println(isSummer);
+
+            //long
+            long phoneNumber = 22334455;
+            //float
+            float interestRate = 4.5f;
+
+            //arithmetic operators
+    int a = 10;
+    int b = 5;
+    int sum = a + b;
+        System.out.println(sum);
+
+        int c = 7;
+        int d = 5;
+        int difference = c - d;
+        System.out.println(difference);
+
+        int e = 5;
+        int f = 6;
+        int result = e * f;
+        System.out.println(result);
+
+        //divide
+        int num1 = 10;
+        int num2 = 1;
+        int division = num1 / num2;
+        System.out.println(division);
+
+        double num3 = 10;
+        double num4 = 3;
+        double divResult = num3 / num4;
+        System.out.println(divResult);
+
+        // % (reminder of a division)
+
+        int g = 12;
+        int h = 5;
+        int reminder = g % h;
+        System.out.println(reminder);
+    }
+}
